9df7d2e71773d909b5e2dd5dfa076a69c1496522
NAYEEM65 <mathnayeem054@gmail.com>
2022-11-28 23:17:29 +0600

    server error handled

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const stripe = require('stripe')(
     'sk_test_51M8SDWAdpdqyZqEI8qp8BeNGgnNjw53ozYEq95sRoZGRlB37xbeBKfcrAPoMO19kdR82ryYjyFC8MS65mEHafQtd00TN7zMGd8',
 );
 require('dotenv').config();
+// Handling Uncaught Exception
+process.on('uncaughtException', (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+});
 const cookieParser = require('cookie-parser');
 // middleware
 app.use(express.json());
@@ -251,4 +257,14 @@ async function run() {
     }
 }
 run().catch((err) => console.log(err));
+
 app.listen(port, console.log('listing on port ' + port));
+
+process.on('unhandledRejection', (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+    server.close(() => {
+        process.exit(1);
+    });
+});
